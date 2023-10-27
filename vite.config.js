b0d5fa4a62ecc1eb9979a643e59a500906126081
kumar-shivang <shivangkumar1857@gmail.com>
2023-10-27 17:30:32 +0530

    Vite config file

---
+++ vite.config.js
@@ -0,0 +1,72 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['./src/assets/favicon.ico', './src/assets/pwa-192x192.png', './src/assets/pwa-512x512.png'],
+      manifest: {
+        name: 'Grocery Store',
+        short_name: 'GroceryStore',
+        description: 'This is a simple grocery store',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './src/assets/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: './src/assets/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    }),
+    basicSsl()
+
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    }
+  },
+  server:{
+    port: 8080,
+    strictPort: true,
+    host: true,
+    https: true
+  },
+  optimizeDeps: {
+    include: ['vue', 'vue-router', 'pinia']
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+
+    },
+    manifest: true,
+
+    }
+})
