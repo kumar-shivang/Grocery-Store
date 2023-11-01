2a16d2844a6e31374336c6b8aac29527d1dab03a
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-01 18:33:55 +0530

    Add new LoginView component for UI experience
    Introduced a new 'LoginView' component in the application. This component includes LoginForm and uses base store for state management. This approach allows us to separate the presentation layer from the business logic, providing a more organized structure, improving maintainability, and enhancing user experience. The changes also include styles scoped to the 'LoginView' component giving us control over the layout and look for the login feature, ensuring a consistent application

---
+++ src/views/LoginView.vue
@@ -0,0 +1,31 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import { useBaseStore } from '@/stores/baseStore'
+import LoginForm from '@/components/LoginForm.vue'
+export default {
+  name: 'LoginView',
+  components: {
+    LoginForm
+  },
+  data: () => ({
+    baseStore: useBaseStore()
+  })
+}
+</script>
+
+<template>
+  <div class="login-form">
+    <LoginForm type="user" />
+  </div>
+</template>
+
+<style scoped>
+.login-form {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
