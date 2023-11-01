58d346f0ace7d9373ffd34042b1adbdaffbc47d2
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-01 18:34:34 +0530

    Change via CSS and library imports to improve UI
    Refactored main.js to swap out 'vuetify' in favor of 'bootstrap' and 'popper' libraries to streamline the styling and improve the user interface (UI). Also replaced the 'materialdesignicons.css' with custom 'styles.scss' for more control over application design. Separated out the router into its own file to better encapsify routing logic. These changes should lead to a more responsive, intuitive, and user-friendly UI.

--- src/main.js
+++ src/main.js
@@ -1,30 +1,17 @@
 import './assets/main.css'
-import '@mdi/font/css/materialdesignicons.css'
+import './scss/styles.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import * as bootstrap from 'bootstrap'
+import * as popper from '@popperjs/core'
 
 import App from './App.vue'
-import router from './router'
-
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import "@mdi/font/css/materialdesignicons.css";
-import "@fortawesome/fontawesome-free/css/all.css";
-
+import router from './router/router'
 
 const app = createApp(App)
-const vuetify = createVuetify({
-    components,
-    directives,
-})
 
 app.use(createPinia())
 app.use(router)
-app.use(vuetify)
-
 
 app.mount('#app')
