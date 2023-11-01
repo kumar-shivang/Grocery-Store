cf9585b5e16bbdaff70194e56765661238639f8e
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-01 18:34:45 +0530

    Add Navbar.vue for navigation and search functionality
    Introduced a new component, Navbar.vue, to improve app navigation and introduce a search mechanism. This component contains methods to handle search queries, changing search type between 'Product' and 'Category', and checks for a user's login status. Navbar also features a custom CSS style for more control over UI enhancement. This change was necessary to provide users with app interaction functionality and facilitate navigation across the site.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,123 @@
+<script>
+import { useBaseStore } from '@/stores/baseStore'
+export default {
+  name: 'Navbar',
+  setup() {
+    const store = useBaseStore()
+    console.log('Setting up store in navbar component')
+    return { store }
+  },
+  data() {
+    return {
+      searchForm: {
+        query: '',
+        type: 'Product'
+      }
+    }
+  },
+  methods: {
+    search() {
+      console.log(this.searchForm)
+    },
+    searchTypeChange(type) {
+      this.searchForm.type = type
+    }
+  },
+  computed: {
+    isLoggedIn() {
+      return this.store.isLoggedIn
+    }
+  }
+}
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">
+        <img src="@/assets/logo.svg" alt="logo" />
+        <span>Grocery Store</span>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <form class="d-flex mx-auto" role="search">
+          <input
+            class="form-control"
+            type="search"
+            placeholder="Search product or category"
+            aria-label="Search"
+            spellcheck="false"
+            data-ms-editor="true"
+            v-model="searchForm.query"
+          />
+          <div class="dropdown">
+            <button
+              class="btn btn-outline-success dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {{ searchForm.type }}
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li>
+                <div class="dropdown-item" @click="searchTypeChange('Product')">Product</div>
+              </li>
+              <li>
+                <div class="dropdown-item" @click="searchTypeChange('Category')">Category</div>
+              </li>
+            </ul>
+          </div>
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <router-link to="login" v-if="!isLoggedIn" class="nav-link active"> Login </router-link>
+            <router-link to="logout" v-else class="nav-link active"> Logout </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  padding: 10px 0;
+  width: 99vw;
+  min-height: 60px;
+}
+.navbar-brand {
+  color: #000;
+  font-size: 1.5rem;
+  font-weight: 700;
+  text-decoration: none;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+}
+img {
+  height: 50px;
+  width: 50px;
+  margin-right: 10px;
+}
+form {
+  width: 50%;
+}
+</style>
