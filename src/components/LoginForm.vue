9759d1452c260c82416964d63da195137e0b2447
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-01 18:33:46 +0530

    Enhance LoginForm.vue with validation and improved UX
    Added extensive changes to LoginForm.vue to provide a more user-friendly login process. Firstly, components 'Form', 'Field', and 'ErrorMessage' from 'vee-validate' have been imported to handle form validation. Secondly, added conditions to validate username and password fields. Meanwhile, layout changes like adding labels to fields, restructuring the login button, and general adjustments to the form's styling were made, adding to the user experience and providing a more cohesive look. This update takes the application one step ahead by aiding user navigation and facilitating interactions.

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,37 +1,116 @@
 <script>
+import { Form, Field, ErrorMessage } from 'vee-validate'
+import { useBaseStore } from '@/stores/baseStore'
+import HomeView from '@/views/HomeView.vue'
 export default {
-  name: "LoginForm",
+  name: 'LoginForm',
+  components: {
+    HomeView,
+    Form,
+    Field,
+    ErrorMessage
+  },
   props: {
     type: {
       type: String,
       required: true,
       validator: (value) => {
-        return ["admin","user","manager"].includes(value);
-        }
-      },
-    },
-  methods: {
-    login() {
-      console.log("ok");
+        return ['user', 'admin', 'manager'].includes(value)
+      }
     }
   },
-  data: () => ({
-    username: '',
-    password: '',
-  }),
-
+  data() {
+    return {
+      store: useBaseStore(),
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login() {
+      console.log(this.form)
+    },
+    validateUsername(username) {
+      if (!username) {
+        return 'Username is required'
+      } else if (username.length < 4) {
+        return 'Username must be at least 4 characters long'
+      } else if (username.length > 20) {
+        return 'Username must be at most 20 characters long'
+      } else {
+        return true
+      }
+    },
+    validatePassword(password) {
+      if (!password) {
+        return 'Password is required'
+      } else if (password.length < 8) {
+        return 'Password must be at least 8 characters long'
+      } else if (password.length > 20) {
+        return 'Password must be at most 20 characters long'
+      } else {
+        return true
+      }
+    }
+  }
 }
 </script>
-
 <template>
-  <v-form>
-    <v-text-field label="username" v-model="username"></v-text-field>
-    <v-text-field label="password" v-model="password"></v-text-field>
-    <v-btn @click.prevent="login">Submit</v-btn>
-  </v-form>
-
+  <Form class="form-control d-flex flex-column" @submit="login">
+    <div class="mx-auto">
+      <h1>Login</h1>
+    </div>
+    <div class="form-group">
+      <label for="username" class="form-label mb-1">Username</label>
+      <Field
+        name="username"
+        label="Username"
+        type="text"
+        v-model="form.username"
+        :rules="validateUsername"
+        class="form-control mb-3"
+        id="username"
+      />
+      <div class="mb-2">
+        <ErrorMessage name="username" class="text-danger" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="password" class="form-label mb-1">Password</label>
+      <Field
+        name="password"
+        label="Password"
+        type="password"
+        v-model="form.password"
+        :rules="validatePassword"
+        class="form-control mb-3"
+        id="password"
+      />
+      <div class="mb-2">
+        <ErrorMessage name="password" class="text-danger" />
+      </div>
+    </div>
+    <div class="d-flex flex-column align-items-center">
+      <button class="btn btn-lg btn-primary w-75 mb-2" @click="login">Login</button>
+      <button
+        class="btn btn-lg btn-light btn-outline-primary w-75 mb-2 btn"
+        @click="$router.push('register')"
+      >
+        Sign Up
+      </button>
+    </div>
+  </Form>
 </template>
 
 <style scoped>
-
+.form-control {
+  width: 50%;
+  margin: auto;
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
 </style>
