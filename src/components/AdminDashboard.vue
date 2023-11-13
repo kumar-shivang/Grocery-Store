e8191bab76736ed740b551b6fd41a67baf19fd13
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-13 15:29:23 +0530

    Implement AdminDashboard.vue component for admin functionality
    This commit introduces adminDashboard.vue, a new Vue.js component designed to be the primary interface for administrators. It leverages baseStore and adminStore to manage tokens and fetch manager requests, providing the groundwork for further admin-specific functionalities. The component currently includes barebone template views and styling. This is a crucial step in building a dedicated, full-featured admin dashboard.

---
+++ src/components/AdminDashboard.vue
@@ -0,0 +1,41 @@
+<script>
+import { useBaseStore } from '@/stores/baseStore'
+import { useAdminStore } from '@/stores/adminStore'
+export default {
+  name: 'AdminDashboard',
+  setup() {
+    const baseStore = useBaseStore()
+    const adminStore = useAdminStore()
+    adminStore.setToken(baseStore.access_token)
+    return { baseStore, adminStore }
+  },
+  data: () => ({
+    baseStore: useBaseStore(),
+    adminStore: useAdminStore()
+  }),
+  beforeMount() {
+    this.adminStore.fetchManagerRequests()
+  }
+}
+</script>
+
+<template>
+  <div id="leftHalf" class="w-50">
+    <h1>Left</h1>
+  </div>
+  <div id="rightHalf" class="w-50">
+    <h1>Right</h1>
+  </div>
+</template>
+
+<style scoped>
+#leftHalf {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  background-color: red;
+}
+</style>
