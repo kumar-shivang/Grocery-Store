5218355c9447f072c99e05df569446212fc479a4
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-13 15:30:05 +0530

    Add access_token and managerRequests to adminstore.js
    Enhanced adminStore.js to handle secure management tasks. Following changes were implemented:
    
    - Added 'access_token' and 'managerRequests' to the adminStore state object to ensure secure communication with the backend and keep track of manager requests.
    
    - Introduced new actions 'setToken' and 'fetchManagerRequests'. 'setToken' is responsible for setting the access token used for secure API calls. 'fetchManagerRequests' is an async function which fetches manager requests from the backend API when the access token is present.
    
    These changes enable secure transmission of manager request data between frontend and backend, laying foundation for new management features.

--- src/stores/adminStore.js
+++ src/stores/adminStore.js
@@ -7,7 +7,9 @@ export const useAdminStore = defineStore({
       id: '',
       username: '',
       email: ''
-    }
+    },
+    access_token: '',
+    managerRequests: []
   }),
   getters: {
     getAdmin() {
@@ -17,6 +19,24 @@ export const useAdminStore = defineStore({
   actions: {
     setAdmin(admin) {
       this.admin = admin
+    },
+    setToken(token) {
+      this.access_token = token
+    },
+    async fetchManagerRequests() {
+      if (this.access_token) {
+        const response = await fetch('http://localhost:5000/api/admin/get_manager_requests', {
+          headers: {
+            Authorization: `Bearer ${this.access_token}`
+          },
+          method: 'GET',
+          mode: 'cors'
+        })
+        if (response.ok) {
+          const data = await response.json()
+          this.managerRequests = data.manager_requests // manager_requests is a list of objects
+        }
+      }
     }
   }
 })
