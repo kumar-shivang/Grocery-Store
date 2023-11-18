d66d8fd16ce2954aa53f0aa58e526627cea762b2
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-18 13:48:29 +0530

    Commit message:
    Updated cookie handling to accommodate user type
    
    Modified cookie handling functions in baseStore.js to include 'type' along with 'access_token'. The functions changed include getCookie, setCookie, and deleteCookie. Now, both 'token' and 'type' are read and written to cookies. This allows for more granular control and customization of user experiences based on their type, improving both security and functionality. This change also impacts the 'checkLogin', 'setAccessToken', and 'fetchAccessToken' functions that involve cookie usage.

--- src/stores/baseStore.js
+++ src/stores/baseStore.js
@@ -2,35 +2,42 @@ import { defineStore } from 'pinia'
 
 function getCookie() {
   let token = document.cookie.split(';').some((item) => item.trim().startsWith('access_token='))
-  if (token) {
+  let type = document.cookie.split(';').some((item) => item.trim().startsWith('type='))
+  if (token && type) {
     token = document.cookie
       .split('; ')
       .find((row) => row.startsWith('access_token'))
       .split('=')[1]
+    type = document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('type'))
+      .split('=')[1]
   } else {
     token = ''
+    type = 'user'
   }
-  return token
+  return [token, type]
 }
-function setCookie(token) {
-  document.cookie = `access_token=${token}; path=/; SameSite=None; Secure;`
+function setCookie(token, type) {
+  document.cookie = `access_token=${token};type=${type}; path=/; SameSite=None; Secure;HttpOnly;`
 }
 
 function deleteCookie() {
-  document.cookie = `access_token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;`
+  document.cookie = `access_token=;type=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;`
 }
 
 export const useBaseStore = defineStore('base', {
   state: () => {
     return {
-      access_token: getCookie(),
+      access_token: getCookie()[0],
       user: {
         id: '',
         username: '',
         email: ''
       },
       isLogged: false,
-      loginError: ''
+      loginError: '',
+      type: getCookie()[1]
     }
   },
   getters: {
@@ -47,7 +54,7 @@ export const useBaseStore = defineStore('base', {
 
   actions: {
     checkLogin() {
-      this.access_token = getCookie()
+      ;[this.access_token, this.type] = getCookie()
       if (this.access_token !== '') {
         this.isLogged = true
       }
@@ -62,31 +69,34 @@ export const useBaseStore = defineStore('base', {
       this.isLogged = false
       deleteCookie()
     },
-    setAccessToken(state, token) {
+    setAccessToken(state, token, type) {
       this.access_token = token
-      setCookie(token)
+      setCookie(token, type)
     },
     async fetchAccessToken(username, password, type) {
-      this.access_token = getCookie()
+      ;[this.access_token, this.type] = getCookie()
       if (this.access_token === '') {
-        const response = await fetch('http://localhost:5000/api/login/' + type, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            username: username,
-            password: password
+        try {
+          const response = await fetch('http://localhost:5000/api/login/' + type, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              username: username,
+              password: password
+            })
           })
-        })
-        const data = await response.json()
-        if (data.access_token) {
+          if (!response.ok) {
+            throw new Error('Something went wrong!')
+          }
+          const data = await response.json()
           this.access_token = data.access_token
-          setCookie(data.access_token)
+          this.type = type
+          setCookie(data.access_token, type)
           this.isLogged = true
-          this.loginError = ''
-        } else {
-          this.showError(data.message)
+        } catch (error) {
+          console.log(error)
         }
       } else {
         this.isLogged = true
