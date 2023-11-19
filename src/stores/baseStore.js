b8b30e80fb020262d13e9ac592633ec469750253
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-19 16:35:54 +0530

    Refine cookie management and user login process
    In 'src/stores/baseStore.js', cookie handling was refined to manage 'access_token' and 'login_type' identifiers effectively. This involved updating the methods 'getCookie', 'setCookie', and 'deleteCookie'. Concurrently, enhancements were made to the methods 'checkLogin', 'setAccessToken', and 'fetchAccessToken' to improve the user login process. These changes offer enhanced user experience control and strengthened security measures.

--- src/stores/baseStore.js
+++ src/stores/baseStore.js
@@ -1,29 +1,34 @@
 import { defineStore } from 'pinia'
 
 function getCookie() {
-  let token = document.cookie.split(';').some((item) => item.trim().startsWith('access_token='))
-  let type = document.cookie.split(';').some((item) => item.trim().startsWith('type='))
-  if (token && type) {
-    token = document.cookie
+  let tokenExists = document.cookie
+    .split(';')
+    .some((item) => item.trim().startsWith('access_token='))
+  let typeExists = document.cookie.split(';').some((item) => item.trim().startsWith('login_type='))
+  if (tokenExists && typeExists) {
+    let access_token = document.cookie
       .split('; ')
       .find((row) => row.startsWith('access_token'))
       .split('=')[1]
-    type = document.cookie
+    let type = document.cookie
       .split('; ')
-      .find((row) => row.startsWith('type'))
+      .find((row) => row.startsWith('login_type'))
       .split('=')[1]
+    return [access_token, type]
   } else {
-    token = ''
-    type = 'user'
+    return ['', 'user']
   }
-  return [token, type]
 }
 function setCookie(token, type) {
-  document.cookie = `access_token=${token};type=${type}; path=/; SameSite=None; Secure;HttpOnly;`
+  let expires = new Date()
+  expires.setTime(expires.getTime() + 24 * 60 * 60 * 1000)
+  document.cookie = `access_token=${token}; path=/; expires=${expires.toUTCString()}`
+  document.cookie = `login_type=${type}; path=/; expires=${expires.toUTCString()}`
 }
 
 function deleteCookie() {
   document.cookie = `access_token=;type=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;`
+  document.cookie = `login_type=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;`
 }
 
 export const useBaseStore = defineStore('base', {
@@ -48,16 +53,22 @@ export const useBaseStore = defineStore('base', {
       return state.user
     },
     getIsLogged(state) {
-      return state.isLogged
+      if (state.access_token !== '') {
+        return true
+      }
+      return false
     }
   },
 
   actions: {
     checkLogin() {
       ;[this.access_token, this.type] = getCookie()
+      console.log(this.access_token, this.type)
       if (this.access_token !== '') {
         this.isLogged = true
+        return true
       }
+      return false
     },
     logout() {
       this.access_token = ''
@@ -66,47 +77,63 @@ export const useBaseStore = defineStore('base', {
         username: '',
         email: ''
       }
+      this.type = 'user'
       this.isLogged = false
       deleteCookie()
+      console.log('logged out')
     },
     setAccessToken(state, token, type) {
       this.access_token = token
+      this.type = type
       setCookie(token, type)
+      this.isLogged = true
     },
     async fetchAccessToken(username, password, type) {
+      console.log('fetching access token')
+      console.log(username, password, type)
       ;[this.access_token, this.type] = getCookie()
-      if (this.access_token === '') {
-        try {
-          const response = await fetch('http://localhost:5000/api/login/' + type, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              username: username,
-              password: password
-            })
-          })
-          if (!response.ok) {
-            throw new Error('Something went wrong!')
-          }
-          const data = await response.json()
+      if (this.access_token !== '' && this.type === type) {
+        console.log('access token exists')
+        this.isLogged = true
+        setCookie(this.access_token, type)
+        return true
+      } else if (this.access_token !== '' && this.type !== type) {
+        console.log('access token exists but type is different')
+        this.logout()
+        return false
+      } else {
+        console.log('access token does not exist')
+        console.log(type)
+        const response = await fetch('http://localhost:5000/api/login/' + type, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ username, password })
+        })
+        const data = await response.json()
+        console.log('response received')
+        console.log(data)
+        if (data.access_token) {
+          console.log('access token received')
           this.access_token = data.access_token
           this.type = type
-          setCookie(data.access_token, type)
           this.isLogged = true
-        } catch (error) {
-          console.log(error)
+          setCookie(data.access_token, type)
+          return true
+        } else {
+          console.log('access token not received')
+          this.showError(data.message)
+          console.log(data)
+          return false
         }
-      } else {
-        this.isLogged = true
       }
     },
     showError(error) {
       this.loginError = error
       setTimeout(() => {
         this.loginError = ''
-      }, 3000)
+      }, 5000)
     }
   }
 })
