da780c2225428a22d2ea9a87694b70ce59622945
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-20 18:07:08 +0530

    Update Navigation Bar
    Reduced clutter in Navbar.vue file by removing the 'Home' link. This streamlined design allows users to focus on critical actions like login/logout.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -86,9 +86,6 @@ export default {
         </form>
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
-          </li>
-          <li class="nav-item">
             <router-link to="login" v-if="!isLoggedIn" class="nav-link active"> Login </router-link>
             <div v-else class="nav-link active" @click="logout">Logout</div>
           </li>
