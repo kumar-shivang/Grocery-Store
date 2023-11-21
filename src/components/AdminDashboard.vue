e129dbd51fc59c978bd49c7c1622618116222c89
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-21 14:45:23 +0530

    Refactor AdminDashboard.vue layout and styles
    The layout of AdminDashboard.vue has been modified from a "half-and-half" design to a more comprehensive layout. This change provides a more intuitive and functional design, enhancing user experience. Additionally, CSS styles have been updated for better visual effect and readability, it also ensures consistency. New variables (var(--app-width) and var(--main-height)) have been employed for responsive design and to maintain site-wide layout consistency.

--- src/components/AdminDashboard.vue
+++ src/components/AdminDashboard.vue
@@ -20,22 +20,60 @@ export default {
 </script>
 
 <template>
-  <div id="leftHalf" class="w-50">
-    <h1>Left</h1>
-  </div>
-  <div id="rightHalf" class="w-50">
-    <h1>Right</h1>
+  <div id="dashboard" class="d-flex flex-row m-0">
+    <div id="left">
+      <h1>Left</h1>
+    </div>
+    <div id="right">
+      <div id="top-right">
+        <h1>Top</h1>
+      </div>
+      <div id="bottom-right">
+        <h1>Bottom</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-#leftHalf {
-  width: 100%;
+#dashboard {
+  width: var(--app-width);
+  height: var(--main-height);
+}
+#left {
+  width: 50%;
   height: 100%;
+  background-color: red;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: flex-start;
-  background-color: red;
+  justify-content: center;
+}
+#right {
+  width: 50%;
+  height: 100%;
+  background-color: blue;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+#top-right {
+  width: 100%;
+  height: 50%;
+  background-color: green;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+#bottom-right {
+  width: 100%;
+  height: 50%;
+  background-color: yellow;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
