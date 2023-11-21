ff8190eab33d389a42149db662d6e5ea79129ccb
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-21 21:54:30 +0530

    Add CategoryForm component for admin dashboard
    Introduced a new component, CategoryForm, to allow the admin to create new categories directly from the admin dashboard. The new component includes form fields for category name and description, and calls methods from the adminStore on form submission. Designed using vee-validate for form validation. This enhancement makes the process of category management more efficient and user-friendly for admin users.

---
+++ src/components/Admin Dashboard Components/CategoryForm.vue
@@ -0,0 +1,69 @@
+<script>
+import { useAdminStore } from '@/stores/adminStore'
+import { Form, Field, ErrorMessage } from 'vee-validate'
+export default {
+  name: 'CategoryForm',
+  setup() {
+    const adminStore = useAdminStore()
+    return { adminStore }
+  },
+  data() {
+    return {
+      category_name: '',
+      category_description: ''
+    }
+  },
+  components: {
+    Form,
+    Field,
+    ErrorMessage
+  },
+  methods: {
+    createCategory() {
+      this.adminStore.createCategory(this.category_name, this.category_description)
+    }
+  }
+}
+</script>
+
+<template>
+  <h3 class="mx-auto">Create Category</h3>
+
+  <Form class="form" @submit="createCategory">
+    <div class="form-group">
+      <label for="category_name">Category Name</label>
+      <Field
+        name="category_name"
+        label="Category Name"
+        v-model="category_name"
+        type="text"
+        class="form-control-sm"
+      />
+    </div>
+    <div class="form-group">
+      <label for="category_description">Category Description</label>
+      <Field
+        name="category_description"
+        label="Category Description"
+        v-model="category_description"
+        class="form-control-sm"
+        type="text"
+      />
+    </div>
+    <button class="btn btn-primary mx-auto" type="submit">Create Category</button>
+  </Form>
+</template>
+
+<style scoped>
+.form {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: end;
+}
+.form-group {
+  margin-right: 33%;
+}
+</style>
