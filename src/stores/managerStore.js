d565ca6f7b6550e382c7c4f53a0bb19cb345b398
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-26 18:01:35 +0530

    Update managerStore.js to support token-based fetch requests
    The managerStore.js has been updated to include `fetchProducts` and `fetchCategories` functions which use token-based authentication. These new actions are set up to fetch product and category data from the server using the bearer token stored in the state. This change optimizes the data fetching process by utilizing the security benefits of token-based authentication and also sets the groundwork for adding more secure data fetching functions in the future.

--- src/stores/managerStore.js
+++ src/stores/managerStore.js
@@ -23,8 +23,45 @@ export const useManagerStore = defineStore({
     }
   },
   actions: {
-    fetchManager() {
-      return true
+    setToken(token) {
+      this.access_token = token
+    },
+    async fetchProducts() {
+      if (this.access_token) {
+        const response = await fetch('http://localhost:5000/api/manager/get_products', {
+          headers: {
+            Authorization: `Bearer ${this.access_token}`
+          },
+          method: 'GET',
+          mode: 'cors'
+        })
+        let data = await response.json()
+        if (response.ok) {
+          this.products = data.products
+          console.log(data.products)
+        } else {
+          console.log(data)
+        }
+      } else {
+        console.log('no token')
+      }
+    },
+    async fetchCategories() {
+      if (this.access_token) {
+        const response = await fetch('http://localhost:5000/api/user/get_categories', {
+          method: 'GET',
+          mode: 'cors'
+        })
+        let data = await response.json()
+        if (response.ok) {
+          this.categories = data.categories
+          console.log(data.categories)
+        } else {
+          console.log(response)
+        }
+      } else {
+        console.log('no token')
+      }
     }
   }
 })
