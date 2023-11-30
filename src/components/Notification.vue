11e4a16fe6ea00efc51d7d07483653e93ae7ce66
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-30 15:17:03 +0530

    Add Notification component with alert system\n
    Implemented a new 'Notification' component which is integrated into the 'App' component as well as the 'baseStore'. This component accepts props to generate notifications alerts based on type attributes like 'success', 'danger', and 'info'. This aims to enhance user interaction and user experience by providing appropriate, timely feedback to the user. A hide method is also included to allow users to dismiss the notification.

---
+++ src/components/Notification.vue
@@ -0,0 +1,66 @@
+<script>
+import { useBaseStore } from '@/stores/baseStore'
+export default {
+  name: 'Notification',
+  setup() {
+    let store = useBaseStore()
+    return { store }
+  },
+  props: {
+    message: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true,
+      validator: (value) => {
+        return ['success', 'danger', 'info'].includes(value)
+      },
+      default: 'success'
+    },
+    got_response: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    hide() {
+      this.store.hideNotification()
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="alert notification" :class="'alert-' + type">
+    <h5 class="alert-heading" v-if="type === 'danger'">Warning</h5>
+    <h5 class="alert-heading" v-else-if="type === 'success'">Success</h5>
+    <h5 class="alert-heading" v-else-if="type === 'info'">Info</h5>
+    {{ message }}
+    <i class="bi bi-x-circle-fill text-danger" @click="hide" />
+  </div>
+</template>
+
+<style scoped>
+.notification {
+  position: fixed;
+  /* this should be displayed in bottom right corner*/
+  bottom: 20px;
+  right: 20px;
+  z-index: 1000;
+  padding: 2rem;
+}
+.bi {
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+.alert-danger {
+  border: 1px solid red;
+}
+.alert-success {
+  border: 1px solid green;
+}
+</style>
