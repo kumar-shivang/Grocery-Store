804996660780d59b7f733324cb0fb9d936fc1b35
kumar-shivang <shivangkumar1857@gmail.com>
2023-12-02 14:37:35 +0530

    Refactor main.js for optimal code organization
    The import statement of BootStrap was moved for a neater code organization. The calls for app.use() have been chained for more readable and concise code without altering the current functionality.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
 import './assets/main.css'
 import './scss/styles.scss'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import * as bootstrap from 'bootstrap'
-import * as popper from '@popperjs/core'
-
 import App from './App.vue'
 import { router } from './router/router'
 
+import * as bootstrap from 'bootstrap'
+
 const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.use(createPinia()).use(router).mount('#app')
