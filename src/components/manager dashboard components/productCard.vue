908a864d4a16920d6ebea12efe776bd326ede74f
kumar-shivang <shivangkumar1857@gmail.com>
2023-12-02 14:40:32 +0530

    Update productCard.vue to add product editing functionalities
    Enhanced the `productCard.vue` component to include features for editing product information and stock. Users can now edit product name, description, and rate directly from the product card. Additionally, functionality for editing the current stock for a product has also been added. Styling adjustments and necessary imports are also included for these new features. These changes allow managers to easily update product details and inventory right from the dashboard, improving efficiency and usability of the application.

--- src/components/manager dashboard components/productCard.vue
+++ src/components/manager dashboard components/productCard.vue
@@ -1,6 +1,20 @@
 <script>
+import { Dropdown } from 'bootstrap'
 export default {
   name: 'productCard',
+  data: () => ({
+    editing: false,
+    editingProduct: {
+      id: null,
+      name: '',
+      description: '',
+      rate: ''
+    },
+    addStock: {
+      id: null,
+      stock: ''
+    }
+  }),
   props: {
     product: {
       type: Object,
@@ -26,6 +40,29 @@ export default {
   methods: {
     toURL(string) {
       return 'http://localhost:5000/' + string
+    },
+    async updateProduct() {},
+    editProduct() {
+      this.editing = true
+      this.editingProduct.id = this.product.id
+      this.editingProduct.name = this.product.name
+      this.editingProduct.description = this.product.description
+      this.editingProduct.rate = this.product.rate
+    },
+    cancelEdit() {
+      this.editing = false
+      this.editingProduct.id = null
+      this.editingProduct.name = ''
+      this.editingProduct.description = ''
+      this.editingProduct.rate = ''
+    },
+    editStock() {
+      this.addStock.id = this.product.id
+      this.addStock.stock = this.product.current_stock
+    },
+    cancelStock() {
+      this.addStock.id = null
+      this.addStock.stock = ''
     }
   }
 }
@@ -33,32 +70,195 @@ export default {
 
 <template>
   <div class="card">
-    <img class="card-img-top img" :src="toURL(product.image.image_path)" />
-    <div class="card-body">
-      <h3 class="card-title">
-        {{ product.name }}
-      </h3>
-      <div class="card-subtitle">
-        {{ product.description }}
+    <div class="card-header">
+      {{ product.category.category_name }}
+      <div class="dropstart" v-if="!editing && !addStock.id">
+        <i class="bi bi-three-dots" data-bs-toggle="dropdown" aria-expanded="false"> </i>
+        <ul class="dropdown-menu">
+          <li><p class="dropdown-item" @click="editProduct">Edit</p></li>
+          <li><p class="dropdown-item" @click="editStock">Edit Stock</p></li>
+          <li data-bs-toggle="modal" data-bs-target="#deleteModal">
+            <p class="dropdown-item text-danger">Delete</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <img class="card-img img" :src="toURL(product.image.image_path)" />
+    <div v-if="!editing && !addStock.id">
+      <div class="card-body" v-if="!editing && !addStock.id">
+        <h3 class="card-title">
+          {{ product.name }}
+        </h3>
+        <div class="card-subtitle">
+          {{ product.description }}
+        </div>
+        <hr />
+        <div class="card-text stock">{{ product.current_stock }} {{ product.unit }} in stock</div>
       </div>
       <div class="card-footer">
-        <h3 style="display: inline">{{ product.rate }}Rs</h3>
+        <span class="rate" style="display: inline">{{ product.rate }}₹</span>
         per {{ product.unit }}
+      </div>
+    </div>
+    <div v-else-if="editing">
+      <div class="card-body">
+        <label class="form-label" for="name">New Product Name</label>
+        <input
+          type="text"
+          v-model="editingProduct.name"
+          class="form-control"
+          name="name"
+          autocomplete="false"
+        />
+        <label class="form-label" for="Description">New Description</label>
+        <input
+          type="text"
+          v-model="editingProduct.description"
+          class="form-control"
+          name="description"
+        />
+        <label class="form-label" for="rate">New Rate</label>
+        <input
+          type="number"
+          v-model.number="editingProduct.rate"
+          class="form-control"
+          name="rate"
+          autocomplete="false"
+          min="0.5"
+          step="0.5"
+        />
+      </div>
+
+      <div class="card-footer">
+        <button class="btn btn-success" @click="editing = false">Save</button>
+        <button class="btn btn-danger" @click="editing = false">Cancel</button>
+      </div>
+    </div>
+    <div v-else-if="addStock.id">
+      <div class="card-body">
+        <h3 class="card-title">
+          {{ product.name }}
+        </h3>
+        <label class="form-label" for="stock">New Stock</label>
+        <input
+          type="number"
+          v-model.number="addStock.stock"
+          class="form-control"
+          name="stock"
+          autocomplete="false"
+          min="0"
+          step="1"
+        />
+        <div class="buttons">
+          <button class="btn btn-success" @click="addStock.stock += 1">
+            <i class="bi bi-plus-square text-white"></i>
+          </button>
+          <button
+            class="btn btn-danger"
+            @click="addStock.stock = addStock.stock > 1 ? addStock.stock - 1 : addStock.stock"
+            :disabled="addStock.stock <= 1"
+          >
+            <i class="bi bi-dash-square text-white"></i>
+          </button>
+        </div>
+      </div>
+      <div class="card-footer">
+        <button class="btn btn-success" @click="editStock = false">Save</button>
+        <button class="btn btn-danger" @click="cancelStock">Cancel</button>
+      </div>
+    </div>
+  </div>
+  <div
+    class="modal fade"
+    id="deleteModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Request Deletion</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">Are you sure to request deletion of this category?</div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
+            Request Delete
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+@import 'bootstrap-icons/font/bootstrap-icons.css';
 .card {
   width: 15rem;
   margin: 1rem;
   border: 1px solid black;
-  z-index: -1;
 }
 .card-img-top {
   width: 100%;
   height: 15rem;
   object-fit: cover;
+}
+.rate {
+  font-size: 1.5rem;
+}
+.card-footer {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.stock {
+  font-size: 1.2rem;
+  background-color: whitesmoke;
+  border: 1px solid #a3cfbb;
+  padding: 10px;
+  width: 100%;
+}
+.card-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+i {
+  font-size: 1.5rem;
+  color: black;
+  margin-right: 0;
+}
+button:hover {
+  color: red;
+}
+.dropstart {
+  position: relative;
+  display: inline-block;
+}
+.bi-pencil {
+  color: black;
+  zoom: 0.5;
+}
+li {
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.buttons {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 1rem;
 }
 </style>
