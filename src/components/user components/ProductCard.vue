be5b7ac3d1558398dfcf7fb709c65a84b4aa3829
kumar-shivang <shivangkumar1857@gmail.com>
2023-12-04 19:44:06 +0530

    Integrate authentication checks and user experience improvements to ProductCard.vue
    Imported 'baseStore' and 'userStore', set up baseStore and userStore in setup() and used them in a new toCart() method. This method is used to control users' action of adding products to the cart, ensuring only logged-in users can do so. Additionally, the display of product price and current stock information has been improved. The quantity selection feature has also been enhanced; users can control the quantity of the product they want to buy in a more interactive and intuitive way. All of these changes aim to enhance the overall user experience.

--- src/components/user components/ProductCard.vue
+++ src/components/user components/ProductCard.vue
@@ -1,4 +1,6 @@
 <script>
+import { useBaseStore } from '@/stores/baseStore'
+import { useUserStore } from '@/stores/userStore'
 export default {
   name: 'ProductCard',
   data() {
@@ -6,15 +8,55 @@ export default {
       quantity: 1
     }
   },
+  setup() {
+    const baseStore = useBaseStore()
+    const userStore = useUserStore()
+    return { baseStore, userStore }
+  },
   props: {
     product: {
       type: Object,
       required: true
     }
   },
+  computed: {
+    access_token() {
+      return this.baseStore.getAccessToken
+    },
+    loggedIn() {
+      return this.baseStore.getIsLogged
+    }
+  },
   methods: {
     toURL(string) {
       return 'http://localhost:5000/' + string
+    },
+    async toCart() {
+      if (!this.loggedIn) {
+        this.baseStore.showNotification('Please login to continue', 'danger')
+        setTimeout(() => {
+          this.$router.push('/login')
+        }, 3000)
+      } else {
+        const response = await fetch('http://localhost:5000/api/order/place_order', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + this.access_token
+          },
+          body: JSON.stringify({
+            product_id: this.product.id,
+            quantity: this.quantity
+          })
+        })
+        const data = await response.json()
+        if (response.ok) {
+          this.baseStore.showNotification('Order placed successfully', 'success')
+          await this.userStore.fetchProducts()
+        } else {
+          this.baseStore.showNotification(data.message, 'danger')
+        }
+      }
     }
   }
 }
@@ -35,15 +77,31 @@ export default {
       <div class="card-text">
         <p>{{ product.description }}</p>
       </div>
-      <div>{{ product.rate }}Rs Per {{ product.unit }}</div>
+      <div id="rate">
+        <h2 class="d-inline-block">{{ product.rate }}</h2>
+        <h4 class="d-inline-block">Rs</h4>
+        Per {{ product.unit }}
+      </div>
+      <div id="stock">
+        <h4 class="d-inline-block me-2">Stock:</h4>
+        <h4 class="d-inline-block">{{ product.current_stock }} {{ product.unit }}</h4>
+      </div>
       <div class="card-footer">
         <div id="adjust">
-          <i class="bi bi-plus"></i>
-          <span id="quantity">{{ quantity }}</span>
-          <i class="bi bi-dash"></i>
+          <i
+            class="bi bi-plus"
+            @click="quantity = quantity < product.current_stock ? quantity + 1 : quantity"
+          ></i>
+          <span id="quantity-unit" class="d-inline-block">
+            <span id="quantity">{{ quantity }}</span
+            ><span id="unit">{{ product.unit }}</span>
+          </span>
+          <i class="bi bi-dash" @click="quantity = quantity > 1 ? quantity - 1 : quantity"></i>
         </div>
 
-        <button id="addToCart" class="btn btn-outline-dark">Buy <i class="bi bi-cart" /></button>
+        <button id="addToCart" class="btn btn-success">
+          Buy <i class="bi bi-cart" @click="toCart" />
+        </button>
       </div>
     </div>
   </div>
@@ -65,13 +123,23 @@ img {
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+  padding: 0;
 }
 #quantity {
-  margin: 0 1rem;
+  margin: 0;
   font-size: 1.5rem;
+}
+#quantity-unit {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0.1rem;
 }
 #addToCart {
   height: 50%;
+  width: 30%;
+  font-size: 0.9rem;
 }
 .bi-plus:hover {
   cursor: pointer;
@@ -82,5 +150,15 @@ img {
   cursor: pointer;
   background-color: rgb(128, 128, 128);
   color: white;
+}
+.bi {
+  font-size: 1.5rem;
+}
+#adjust {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 70%;
 }
 </style>
