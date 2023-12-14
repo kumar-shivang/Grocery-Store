e41e7c6a9939dbf530b62f905cb7eb7f6b5fde21
kumar-shivang <shivangkumar1857@gmail.com>
2023-12-14 17:01:20 +0530

    Remove preview server setup

--- run preview server.sh
+++
@@ -1,8 +0,0 @@
-echo "Running preview server"
-npm install
-echo "Generating icons"
-pwa-assets-generator -p minimal  ./src/assets/logo.svg
-echo "Generating manifest"
-vite build
-echo "Starting preview server"
-vite preview --host --port 4000
